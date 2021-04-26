1afba7026741e14e1128906fdd9c058c98556d2a
udara <udara.weera@gmail.com>
2021-04-26 11:44:39 -0400

    parse url correctly

--- PlaidLink.tsx
+++ PlaidLink.tsx
@@ -28,7 +28,12 @@ export default function PlaidLink({
   const handleNavigationStateChange = (event: any) => {
     if (event.url.startsWith('plaidlink://')) {
       console.log(event.url)
-      const eventParams = queryString.parse(event.url)
+      const eventParams = queryString.parse(
+        event.url.replace(
+          /plaidlink:\/\/event|plaidlink:\/\/exit|plaidlink:\/\/connected/gi,
+          ''
+        )
+      )
 
       const linkSessionId = eventParams.link_session_id as string
       const mfaType = eventParams.mfa_type as string
