87f36b17e236c692826d0ceedf1f1602479417dd
Jordan Baczuk <jordan.baczuk@gmail.com>
2021-11-04 07:49:28 -0600

    update dependencies, revert to js, simplify. resolves #9

---
+++ Index.jsx
@@ -0,0 +1,92 @@
+import React, { useRef } from "react";
+import { WebView } from "react-native-webview";
+import queryString from "query-string";
+import { LinkErrorCode, LinkErrorType, LinkExitMetadataStatus } from './const'
+
+export default function PlaidLink({ linkToken, onEvent, onExit, onSuccess }) {
+  let webviewRef = useRef();
+
+  const handleNavigationStateChange = (event) => {
+    if (event.url.startsWith("plaidlink://")) {
+      const eventParams = queryString.parse(event.url.replace(/.*\?/, ""));
+
+      const linkSessionId = eventParams.link_session_id;
+      const mfaType = eventParams.mfa_type;
+      const requestId = eventParams.request_id;
+      const viewName = eventParams.view_name;
+      const errorCode = eventParams.error_code;
+      const errorMessage = eventParams.error_message;
+      const errorType = eventParams.error_type;
+      const exitStatus = eventParams.exist_status;
+      const institutionId = eventParams.institution_id;
+      const institutionName = eventParams.institution_name;
+      const institutionSearchQuery = eventParams.institution_search_query;
+      const timestamp = eventParams.timestamp;
+
+      if (event.url.startsWith("plaidlink://event") && onEvent) {
+        onEvent({
+          eventName: eventParams.event_name,
+          metadata: {
+            linkSessionId,
+            mfaType,
+            requestId,
+            viewName,
+            errorCode,
+            errorMessage,
+            errorType,
+            exitStatus,
+            institutionId,
+            institutionName,
+            institutionSearchQuery,
+            timestamp,
+          },
+        });
+      } else if (event.url.startsWith("plaidlink://exit") && onExit) {
+        onExit({
+          error: {
+            errorCode: LinkErrorCode[errorCode],
+            errorMessage: eventParams.error_message,
+            errorType: LinkErrorType[errorType],
+          },
+          metadata: {
+            status: LinkExitMetadataStatus[exitStatus],
+            institution: {
+              id: institutionId,
+              name: institutionName,
+            },
+            linkSessionId,
+            requestId,
+          },
+        });
+      } else if (event.url.startsWith("plaidlink://connected") && onSuccess) {
+        const publicToken = eventParams.public_token;
+        const accounts = JSON.parse(eventParams.accounts);
+        onSuccess({
+          publicToken,
+          metadata: {
+            institution: {
+              id: institutionId,
+              name: institutionName,
+            },
+            accounts,
+            linkSessionId,
+          },
+        });
+      }
+      return false;
+    }
+    return true;
+  };
+
+  return (
+    <WebView
+      source={{
+        uri: `https://cdn.plaid.com/link/v2/stable/link.html?isWebview=true&token=${linkToken}`,
+      }}
+      ref={(ref) => (webviewRef = ref)}
+      onError={() => webviewRef.reload()}
+      originWhitelist={["https://*", "plaidlink://*"]}
+      onShouldStartLoadWithRequest={handleNavigationStateChange}
+    />
+  );
+}
--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 # Expo Plaid Link
 
-[![NPM](https://img.shields.io/badge/npm-1.0.5-blue)](https://www.npmjs.org/@burstware/expo-plaid-link)
+[![NPM](https://img.shields.io/badge/npm-1.1.0-blue)](https://www.npmjs.org/@burstware/expo-plaid-link)
 
 Use the [Plaid Link](https://plaid.com/docs/link/) flow inside your expo app.
 
@@ -19,12 +19,12 @@ Use the [Plaid Link](https://plaid.com/docs/link/) flow inside your expo app.
 ## Installation
 
 ```bash
-yarn add @burstware/expo-plaid-link
+expo install @burstware/expo-plaid-link
 ```
 
 ## Usage
 
-```typescript
+```javascript
 import React from 'react'
 import PlaidLink from '@burstware/expo-plaid-link'
 import Config from './config'
@@ -41,18 +41,4 @@ export default function App() {
 }
 ```
 
-_See `types.ts` for data structure format for `onEvent` (see type `LinkEvent`), `onExit` (see type `LinkExit`), and `onSuccess` (see type `LinkSuccess`) function return values._
-
-## Demo
-
-1. `git clone git@github.com:burstware/expo-plaid-link.git`
-2. Create a file in the root of the project called `config.ts` with a link token:
-
-```
-export default {
-  TEST_LINK_TOKEN: 'link-development-deadbeef-7b58-4877-8b44-123456789abcdef',
-}
-```
-
-3. Update the `main` entry in `package.json` to `"main": "node_modules/expo/AppEntry.js",`
-4. `yarn start`
+_See `const.js` for data structure format for `onEvent` (see type `LinkEvent`), `onExit` (see type `LinkExit`), and `onSuccess` (see type `LinkSuccess`) function return values._
